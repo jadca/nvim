45542fef76c22d5ea5621b86ddb6e1f3ce9292cb
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2024-09-20 09:52:11 -0500

    se agreago retricciones treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -20,7 +20,7 @@ nvim_treesitter.setup {
   highlight = {
     enable = true,
     --disable = {"lua","vim","javascript","typescript","html"},
-    disable = {"lua","vim"},
+    disable = {"lua","vim","http"},
     additional_vim_regex_highlighting =true, 
   },
  incremental_selection = {
