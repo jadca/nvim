01de0fb4beb6d070b4775520746cadbc1f8f48ca
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2024-10-24 23:56:01 -0500

    mas colores para typescript

--- init.lua
+++ init.lua
@@ -1,15 +1,10 @@
-
 -- Cargar las opciones generales
 require("config.options")
-
 -- Cargar las configuraciones de plugins (Lazy.nvim)
 require("plugins")
-
 -- Cargar los mapeos de teclas
 require("config.keymaps")
-
 require("config.mason")
-
 -- Cargar configuraciones de plugins adicionales
 require("config.lsp")
 require("config.cmp")
