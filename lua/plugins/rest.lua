9737511977a282317a3ea22c522eb1a04e8e96b9
Jose Alvaro Del Castilllo Aquino <alvarodelcastillo@outlook.com>
2024-10-25 16:46:23 -0500

    fix

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,2 +1,15 @@
 return {
+    --{
+        --'rest-nvim/rest.nvim',
+        --dependencies = { 'nvim-lua/plenary.nvim' },  -- Asegúrate de que las dependencias están aquí
+        --opts = {
+            --rocks = {
+                --hererocks = true,   -- Activa hererocks para las dependencias
+                --enabled = true      -- Habilita el soporte de rocks en general
+            --},
+        --},
+        --config = function()
+            --require('rest-nvim').setup()
+        --end,
+    --}
 }
